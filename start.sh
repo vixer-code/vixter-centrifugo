98a4dcb6b37e34aa6bd29fc2863c00ebe1a0f5f9
dysph_ <enzoganhito2+1@gmail.com>
2025-09-05 20:15:20 -0300

    a

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Gera config.json real a partir do template e das secrets do Fly.io
-envsubst < /config.json.template > /config.json
-
-# Roda o Centrifugo com o config gerado
-exec centrifugo --config /config.json
